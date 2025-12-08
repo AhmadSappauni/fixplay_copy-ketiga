65a647f7cdf14560e25eb88a103a439a74987080
AhmadSappauni <ahmadsapauni8989@gmail.com>
2025-12-08 11:41:45 +0800

    update pw bos

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -14,7 +14,7 @@ class AdminUserSeeder extends Seeder
             ['email' => 'bos@fixplay.test'], // email unik
             [
                 'name'     => 'Bos Fixplay',
-                'password' => Hash::make('pw-bos'), // ganti ke password yang aman
+                'password' => Hash::make('bismillahfixplay123'), // ganti ke password yang aman
                 'role'     => 'boss', // sesuaikan dengan kolom role di project kamu
             ]
         );
